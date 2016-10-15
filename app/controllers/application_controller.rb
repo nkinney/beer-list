730d01fbc06d7cee8317d9456265d4c78482a50d
Nick Kinney <nkinney1@gmail.com>
2016-10-15 10:29:29 -0400

    added the api/beers route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
+require_relative('../../beer-list')
 class ApplicationController < ActionController::API
+	def beers
+		parsed_beers = ParsedBeers.new
+		parsed_beers.parse_beers_string
+		render json: parsed_beers.get_beers_json
+	end
 	def version
 		render body: "Beer List API"
 	end
