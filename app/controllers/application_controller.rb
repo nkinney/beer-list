55b5fc38e738b89255eae917aad0f0eeedeb4181
Nick Kinney <nkinney1@gmail.com>
2016-10-16 11:16:37 -0400

    Added a route to get teh current user.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,19 @@ class ApplicationController < ActionController::API
 		parsed_beers.parse_beers_string
 		render json: parsed_beers.get_beers_json
 	end
+
+	def get_user
+		user_id = params['user']
+		puts "params: #{params.inspect}"
+		found_user = users.select { |u| u['id'] == user_id }
+		render json: found_user
+	end
+
+	def users
+		users_json_string = '[{ "_id":"56ecc9d751b370b72ed12aa2", "id":"nkinney", "email":"nkinney@gmail.com", "username" : "nkinney", "password" : "$2a$05$IbgElGXHDcw5lZRMFp1lkOlBUikGxXEhcq9CnrIKuCFFJxjC3ISO2", "firstname" : "Nick", "lastname" : "Kinney", "beersTried" : [ ], "__v" : 0 },{ "_id" : "57f38ddee5fa55632fb73c32", "id" : "nick", "email" : "me@my.com", "username" : "nick", "password" : "$2a$05$PSyQ.XWfuXszCCpEm4Lh8.tZNYHMMeF.zN694JZtH4HrkmwBwvnmW", "firstname" : "nick", "lastname" : "kin", "beersTried" : [ ], "__v" : 0 }]'
+		JSON.parse(users_json_string)
+	end
+
 	def version
 		render body: "Beer List API"
 	end
