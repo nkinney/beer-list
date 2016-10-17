9f9275ba7549367fd400d7ac5255c6e3d0d61308
Nick Kinney <nkinney1@gmail.com>
2016-10-16 20:51:04 -0400

    Added mongoid to the app so I can get data from teh database.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,4 @@
-require_relative('../../beer-list')
 class ApplicationController < ActionController::API
-	def beers
-		parsed_beers = ParsedBeers.new
-		parsed_beers.parse_beers_string
-		render json: parsed_beers.get_beers_json
-	end
 
 	def get_user
 		user_id = params['user']
